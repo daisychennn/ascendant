fcdb31bd740b8e67a9e8de9ad197cdd825c3997c
Alexandra Butler <butler0alex@gmail.com>
2024-11-12 21:35:52 -0500

    Adding Nhu's model over to reference in own code

---
+++ lib/models/login_model.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginModel with ChangeNotifier {
+  String _email = '';
+  String _password = '';
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  String get email => _email;
+  String get password => _password;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  set email(String email) {
+    _email = email;
+    notifyListeners();
+  }
+
+  set password(String password) {
+    _password = password;
+    notifyListeners();
+  }
+}
