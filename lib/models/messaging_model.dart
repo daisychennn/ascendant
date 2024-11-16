733ba8cbd14c284fade2e570874a59eabefc06ee
daisychennn <dac229@pitt.edu>
2024-11-16 04:09:38 -0500

    Revert "added some stuff for chat/messaging"
    This reverts commit 22208590004a3c43ecd2ec9f176d30f7b1568310.

--- lib/models/messaging_model.dart
+++ lib/models/messaging_model.dart
@@ -1,19 +1,3 @@
-class UserModel {
-  final String id;
-
-  UserModel({required this.id});
-}
-
-class MessageModel {
-  final String id;
-  final String text;
-  final UserModel author;
-  final int createdAt;
-
-  MessageModel({
-    required this.id,
-    required this.text,
-    required this.author,
-    required this.createdAt,
-  });
+class MessagingModel {
+    String? matchName;
 }
