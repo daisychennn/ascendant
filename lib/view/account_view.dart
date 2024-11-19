d299a6b5136f3b866775f8d48d60e2dfb7718d67
Alexandra Butler <butler0alex@gmail.com>
2024-11-18 22:59:43 -0500

    Cleaned up interface and added fake profiles

--- lib/view/account_view.dart
+++ lib/view/account_view.dart
@@ -12,19 +12,19 @@ class AccountView extends StatefulWidget {
 }
 
 class _AccountView extends State<AccountView> {
-  TextEditingController prompt1 = TextEditingController(text: 'Prompt 1');
+  TextEditingController prompt1 = TextEditingController(text: 'Dating me is like...');
   bool isEnabledP1 = false;
-  TextEditingController answer1 = TextEditingController(text: 'Answer 1');
+  TextEditingController answer1 = TextEditingController(text: 'I\'m not a real person');
   bool isEnabledA1 = false;
 
-  TextEditingController prompt2 = TextEditingController(text: 'Prompt 2');
+  TextEditingController prompt2 = TextEditingController(text: 'Don\'t hate me if I...');
   bool isEnabledP2 = false;
-  TextEditingController answer2 = TextEditingController(text: 'Answer 2');
+  TextEditingController answer2 = TextEditingController(text: 'am a fake profile');
   bool isEnabledA2 = false;
 
-  TextEditingController prompt3 = TextEditingController(text: 'Prompt 3');
+  TextEditingController prompt3 = TextEditingController(text: 'I bet you can\'t...');
   bool isEnabledP3 = false;
-  TextEditingController answer3 = TextEditingController(text: 'Answer 3');
+  TextEditingController answer3 = TextEditingController(text: 'find me! Still not real!');
   bool isEnabledA3 = false;
 
   @override
@@ -35,19 +35,10 @@ class _AccountView extends State<AccountView> {
         child: SingleChildScrollView(
           child: Column(
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
-                  child: Icon(CupertinoIcons.sparkles, color: Colors.yellowAccent, size: 50.0),
-                )
-              ],
-            ),
             Center(
               child:
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 20.0),
+                  padding: const EdgeInsets.fromLTRB(0.0, 10.0, 20.0, 20.0),
                   child: Container(
                       width: 200,
                       height: 200,
@@ -58,98 +49,131 @@ class _AccountView extends State<AccountView> {
                         ),
                         image: const DecorationImage(
                           fit: BoxFit.cover,
-                          image: AssetImage('images/pfp.png')
+                          image: NetworkImage('https://images.unsplash.com/photo-1723200166097-4eed8c141f03?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
                         )
                       )
                 ),
               )
             ),
-            const Text('Name'),
-                    const Text('Age'),
-                    const Text('Sun'),
-                    const Text('Moon'),
-                    const Text('Rising'),
+            // User information that can be edited - will need actual data
             const Row(
-              children: <Widget> [
-              // User Information that can't be edited
-              Expanded(
-                flex: 3,
-                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.person, color: Colors.purple),
+                Text('Natalie NotAName'),
+            ],),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.cake, color: Colors.purple),
+                Text('21'),
+            ],),
+            
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.sunny, color: Colors.yellowAccent),
+                Text('Taurus'),
+            ],),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(CupertinoIcons.moon, color: Colors.yellowAccent),
+                Text('Leo'),
+            ],),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.yellowAccent),
+                Text('Aries'),
+            ],),
+            // Editable User Information
+            Center(
+              child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    
-                  ]
-                ),
-              ),
-              ]),
-              // User information that can be edited
-              Center(
-                // flex: 7,
-                child: Column(
-                children: <Widget>[
-                  // Row(
-                    // children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                      SizedBox(
-                        width: 100,
-                        child: TextField(
-                          controller: prompt1,
-                          enabled: isEnabledP1,
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          isEnabledP1 ? Icons.check : Icons.edit,
-                          size: 25.0
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          if (isEnabledP1) {
-                            isEnabledP1 = false;
-                          }
-                          else {
-                            isEnabledP1 = true;
-                          }
-                        });
-                      }
+                    const Padding(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child: Icon(Icons.question_mark_rounded),
                     ),
-                  ],),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
                     SizedBox(
-                        width: 100,
-                        child: TextField(
-                          controller: answer1,
-                          enabled: isEnabledA1,
-                        ),
+                      width: 150,
+                      child: TextField(
+                        controller: prompt1,
+                        enabled: isEnabledP1,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        style: const TextStyle(color: Colors.black)
                       ),
-                      IconButton(
-                        icon: Icon(
-                          isEnabledA1 ? Icons.check : Icons.edit,
-                          size: 25.0
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          if (isEnabledA1) {
-                            isEnabledA1 = false;
-                          }
-                          else {
-                            isEnabledA1 = true;
-                          }
-                        });
-                      }
                     ),
-                  ],),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+                    IconButton(
+                      icon: Icon(
+                        isEnabledP1 ? Icons.check : Icons.edit,
+                        size: 25.0
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (isEnabledP1) {
+                          isEnabledP1 = false;
+                        }
+                        else {
+                          isEnabledP1 = true;
+                        }
+                      });
+                    }
+                  ),
+                ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 10.0),
+                    child: Icon(Icons.question_answer),
+                  ),
+                  SizedBox(
+                      width: 150,
+                      child: TextField(
+                        controller: answer1,
+                        enabled: isEnabledA1,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        isEnabledA1 ? Icons.check : Icons.edit,
+                        size: 25.0
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (isEnabledA1) {
+                          isEnabledA1 = false;
+                        }
+                        else {
+                          isEnabledA1 = true;
+                        }
+                      });
+                    }
+                  ),
+                ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child: Icon(Icons.question_mark_rounded),
+                    ),
                     SizedBox(
-                        width: 100,
+                        width: 150,
                         child: TextField(
                           controller: prompt2,
                           enabled: isEnabledP2,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          style: const TextStyle(color: Colors.black),
                         ),
                       ),
                       IconButton(
@@ -168,42 +192,56 @@ class _AccountView extends State<AccountView> {
                         });
                       }
                     ),
-                  ]),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 100,
-                        child: TextField(
-                          controller: answer2,
-                          enabled: isEnabledA2,
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          isEnabledA2 ? Icons.check : Icons.edit,
-                          size: 25.0
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          if (isEnabledA2) {
-                            isEnabledA2 = false;
-                          }
-                          else {
-                            isEnabledA2 = true;
-                          }
-                        });
-                      }
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child: Icon(Icons.question_answer),
                     ),
-                  ],),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
                     SizedBox(
-                        width: 100,
+                      width: 150,
+                      child: TextField(
+                        controller: answer2,
+                        enabled: isEnabledA2,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        style: const TextStyle(color: Colors.white)
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        isEnabledA2 ? Icons.check : Icons.edit,
+                        size: 25.0
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (isEnabledA2) {
+                          isEnabledA2 = false;
+                        }
+                        else {
+                          isEnabledA2 = true;
+                        }
+                      });
+                    }
+                  ),
+                ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child: Icon(Icons.question_mark_rounded),
+                    ),
+                    SizedBox(
+                        width: 150,
                         child: TextField(
                           controller: prompt3,
                           enabled: isEnabledP3,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          style: const TextStyle(color: Colors.black)
                         ),
                       ),
                       IconButton(
@@ -222,15 +260,23 @@ class _AccountView extends State<AccountView> {
                         });
                       }
                     ),
-                  ]),
-                  Row(
+                ]),
+                Padding(padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-SizedBox(
-                        width: 100,
+                      const Padding(
+                        padding: EdgeInsets.only(right: 10.0),
+                        child: Icon(Icons.question_answer),
+                      ),
+                      SizedBox(
+                        width: 150,
                         child: TextField(
                           controller: answer3,
                           enabled: isEnabledA3,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          style: const TextStyle(color: Colors.white)
                         ),
                       ),
                       IconButton(
@@ -249,9 +295,10 @@ SizedBox(
                         });
                       }
                     )
-                  ],)
-                ])
-              )
+                  ],),
+                ),
+              ])
+            )
           ],
         ),
       ),
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -16,34 +16,29 @@ class _ProfileView extends State<ProfileView> {
       backgroundColor: const Color.fromRGBO(235, 169, 248, 1.0),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
-                  child: Icon(CupertinoIcons.sparkles, color: Colors.yellowAccent, size: 50.0),
-                )
-              ]
-            ),
             Text(
-              'Name',
-              style: GoogleFonts.prata().merge(const TextStyle(color: Colors.purple, fontSize: 50.0))
+              'Fiona FakePerson',
+              style: GoogleFonts.prata().merge(const TextStyle(color: Colors.purple, fontSize: 40.0)),
+              textAlign: TextAlign.center,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.purpleAccent,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Image.asset(
-                      '',
-                      width: 300
+          Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 20.0),
+                child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purpleAccent,
+                        width: 5,
+                      ),
+                      image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage('https://images.unsplash.com/photo-1632324343640-86af9827dbeb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+                      )
                     )
-                  )
-                )
-              ]
+              ),
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -52,19 +47,19 @@ class _ProfileView extends State<ProfileView> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Icon(Icons.sunny, color: Colors.yellowAccent),
-                    Text('Sun Sign', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white))),
+                    Text('Gemini', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white))),
                 ]),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Icon(CupertinoIcons.moon, color: Colors.yellowAccent),
-                    Text('Moon Sign', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white))),
+                    Text('Virgo', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white))),
                 ]),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Icon(Icons.star, color: Colors.yellowAccent),
-                    Text('Rising Sign', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white)))
+                    Text('Libra', style: GoogleFonts.prata().merge(const TextStyle(color: Colors.white)))
                 ])
               ],
             )
@@ -72,15 +67,15 @@ class _ProfileView extends State<ProfileView> {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        height: 150,
-        color: Colors.purpleAccent,
+        height: 100,
+        color: const Color.fromARGB(178, 223, 64, 251),
         child: Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
               child: IconButton (
                 icon: const Icon(Icons.thumb_up, color: Colors.pink, size: 40),
                 onPressed: () {
@@ -89,7 +84,7 @@ class _ProfileView extends State<ProfileView> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
               child: IconButton (
                 icon: const Icon(Icons.info, color: Colors.white, size: 40),
                 onPressed: () {
@@ -98,9 +93,9 @@ class _ProfileView extends State<ProfileView> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
               child: IconButton (
-                icon: const Icon(Icons.thumb_down, color: Colors.blueAccent, size: 40),
+                icon: const Icon(Icons.thumb_down, color: Colors.lightBlueAccent, size: 40),
                 onPressed: () {
                   setState((){});
                 }
